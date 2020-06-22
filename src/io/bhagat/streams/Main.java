17b99c0c57441e04df650a99c11b4ce9f25d9443
Rishav Bhagat <rishavb123@gmail.com>
2020-06-22 17:42:05 -0400

    added comment

--- src/io/bhagat/streams/Main.java
+++ src/io/bhagat/streams/Main.java
@@ -5,7 +5,9 @@ import io.bhagat.streams.server.Server;
 public class Main {
 
     public static void main(String[] args) {
+         /* Server Testing */
         Server server = new Server(8000, 100);
         server.start();
+
     }
 }
